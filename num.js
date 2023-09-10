be33fac107ccb9900686040ba62612dd64440f4a
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-10 10:30:44 -0700

    finito
    thanks coding artists.. these code alongs are great! Helps me get an understanding of how everything works quickly! Greatly appreciate you!

--- num.js
+++ num.js
@@ -1,5 +1,5 @@
 
-const getFactBtn = document.getElementById('gen-fact-btn')
+const getFactBtn = document.getElementById('get-fact-btn')
 const ranFactBtn = document.getElementById('ran-fact-btn')
 const fact = document.querySelector('.fact')
 let url = "http://numbersapi.com/"
@@ -11,6 +11,8 @@ let fetchFact = (iNumP) => {
     fetch(finalUrl)
     .then(x => x.text())
     .then(y => {
+
+        fact.style.display = "block"
         fact.innerHTML = `<h2>${iNumP}</h2>
         <p>${y}</p>
         
@@ -19,8 +21,39 @@ let fetchFact = (iNumP) => {
     })
 }
 
+let getFact = () => {
 
-fetchFact(18)
+    let num = document.getElementById('num').value
+
+    // check if input is empty or not
+
+
+    if(num) {
+        // check if number is 0 -300 if it is...
+        if (num >=0 && num <= 300) {
+            fetchFact(num);
+        } else {
+            fact.style.display =  "block";
+            fact.innerHTML = `<p class="msg"> Need number 0 - 300</p>`
+        }
+    } else {
+        fact.style.display = "block"
+        fact.innerHTML = `<p class="msg">Input cant be empty</p>`
+    }
+
+}
+
+let getRandomFact = () => {
+
+    let rNum = Math.floor(Math.random() * 301);
+    fetchFact(rNum)
+}
+
+ranFactBtn.addEventListener('click', getRandomFact)
+getFactBtn.addEventListener('click', getFact)
+
+window.addEventListener('load', getRandomFact)
+// fetchFact(18)
 
 
 
