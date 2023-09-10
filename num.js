cc5355e5277103eb7b9f9743e8122a7cfe396fed
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-10 09:39:34 -0700

    le gIt it

--- num.js
+++ num.js
@@ -4,17 +4,23 @@ const ranFactBtn = document.getElementById('ran-fact-btn')
 const fact = document.querySelector('.fact')
 let url = "http://numbersapi.com/"
 
-let fetchFact = () => {
-    let finalUrl = url;
+// i for input ... p for parameter... e.g.(iNumP)
+
+let fetchFact = (iNumP) => {
+    let finalUrl = url + iNumP;
     fetch(finalUrl)
     .then(x => x.text())
     .then(y => {
-        console.log(y)
+        fact.innerHTML = `<h2>${iNumP}</h2>
+        <p>${y}</p>
+        
+        `
+        document.querySelector('.container').append(fact)
     })
 }
 
 
-fetchFact()
+fetchFact(18)
 
 
 
